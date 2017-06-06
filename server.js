b4f7b6f8b59a0b4f29046ad95deb045a7b510f9e
Paton Vinal <paton.vinal@gmail.com>
2017-06-06 13:43:22 -0400

    initial commit and skeleton

---
+++ server.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+
+const app = express();
+app.use(bodyParser.json());
+
+
+
+
+
+
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+         if (err) {
+             return reject(err);
+           }
+         resolve();
+       });
+     });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
